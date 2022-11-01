81aa5daf6459d5a8f832778a8e47c25f47a4ee5d
ash0545 <homeashwinsanthosh@gmail.com>
2022-11-01 17:09:50 +0530

    Add method to apply inputs to board

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -6,11 +6,11 @@ class Board
 
   end
 
-  def win?()
+  def win?() # check if the board contains winning pattern
 
   end
 
-  def create()
+  def create() # 3x3 board numbered from 1-9
     i = 1
     @@board.map! do |row|
       row.map! do |element|
@@ -27,26 +27,31 @@ class Board
     puts "#{@@board[1][0]} | #{@@board[1][1]} | #{@@board[1][2]}"
     puts "--  --  --"
     puts "#{@@board[2][0]} | #{@@board[2][1]} | #{@@board[2][2]}"
-    puts @@board.index(3)
   end
 
   def board_input(x_o, location)
-    coordinates = convert_to_coord(location)
-    p coordinates
+    location = location.to_i
+    coords = convert_to_coord(location)
     case x_o
     when 'x'
-      if @@board[]
-        
+      if @@board[coords[0]][coords[1]] != location
+        puts "Invalid choice"
       else
-        
+        @@board[coords[0]][coords[1]] = 'x'
       end
     when 'o'
+      if @@board[coords[0]][coords[1]] != location
+        puts "Invalid choice"
+      else
+        @@board[coords[0]][coords[1]] = 'o'
+      end
     else 
+      puts "Invalid input"
     end
   end
 
   private
-  def convert_to_coord(location)
+  def convert_to_coord(location) # converts numbers(1- 9) to position on board array
     a, b = 0, 0
     @@board.each do |row|
       if row.include?(location)
@@ -64,6 +69,9 @@ end
 temp = Board.new()
 temp.create
 temp.display_board
-temp.board_input('o', 9)
+puts "Enter location:"
+a = gets
+temp.board_input('o', a)
+temp.display_board
 
 
